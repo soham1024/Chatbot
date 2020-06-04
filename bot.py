98eeea46a6e707b89e386f2c48fd9a5601a3ca63
Soham Mukherjee <42044105+soham1024@users.noreply.github.com>
2020-06-04 21:20:27 +0530

    answer update

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import random
 import string # to process standard python strings
 
 m=open('modules pythons.txt','r',errors = 'ignore')
-f=open('nlp python answer finals.txt','r',errors = 'ignore')
+f=open('python answer.txt','r',errors = 'ignore')
 checkpoint = "./chatbot_weights.ckpt"
 
 raw=f.read() #read the f file --> nlp python answer finals.txt
@@ -41,7 +41,7 @@ Introduce_Ans = ["My name is PyBot.","My name is PyBot you can called me pi.","I
 GREETING_INPUTS = ("hello", "hi","hiii","hii","hiiiii","hiiii", "greetings", "sup", "what's up","whats up","hey")
 GREETING_RESPONSES = ["hi", "hey", "hii there", "hi there", "hello", "I am glad! You are talking to me"]
 Basic_Q = ("what is python ?","what is python","what is python?","what is python.")
-Basic_Ans = "Python is a high-level, interpreted, interactive and object-oriented scripting programming language python is designed to be highly readable It uses English keywords frequently where as other languages use punctuation, and it has fewer syntactical constructions than other languages."
+Basic_Ans = "Python is a high-level, interpreted, interactive and object-oriented scripting programming language. Python is designed to be highly readable It uses English keywords frequently where as other languages use punctuation, and it has fewer syntactical constructions than other languages."
 Basic_Om = ("what is module","what is module.","what is module ","what is module ?","what is module?","what is module in python","what is module in python.","what is module in python?","what is module in python ?")
 Basic_AnsM = ["Consider a module to be the same as a code library.","A file containing a set of functions you want to include in your application.","A module can define functions, classes and variables. A module can also include runnable code. Grouping related code into a module makes the code easier to understand and use."]
 
