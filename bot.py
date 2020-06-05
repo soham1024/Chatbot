a4a519414e5bf4ea4aca38d834ef0337fdbecd4b
Soham Mukherjee <42044105+soham1024@users.noreply.github.com>
2020-06-05 12:14:46 +0530

    answers improved

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ import string # to process standard python strings
 
 m=open('modules pythons.txt','r',errors = 'ignore')
 f=open('python answer.txt','r',errors = 'ignore')
-checkpoint = "./chatbot_weights.ckpt"
 
 raw=f.read() #read the f file --> nlp python answer finals.txt
 raw_one=m.read() #read the m file --> modules pythons.txt
@@ -41,7 +40,7 @@ Introduce_Ans = ["My name is PyBot.","My name is PyBot you can called me pi.","I
 GREETING_INPUTS = ("hello", "hi","hiii","hii","hiiiii","hiiii", "greetings", "sup", "what's up","whats up","hey")
 GREETING_RESPONSES = ["hi", "hey", "hii there", "hi there", "hello", "I am glad! You are talking to me"]
 Basic_Q = ("what is python ?","what is python","what is python?","what is python.")
-Basic_Ans = "Python is a high-level, interpreted, interactive and object-oriented scripting programming language. Python is designed to be highly readable It uses English keywords frequently where as other languages use punctuation, and it has fewer syntactical constructions than other languages."
+Basic_Ans = "Python is a high-level, interpreted, interactive and object-oriented scripting programming language. Python is designed to be highly readable. It uses English keywords frequently where as other languages use punctuation, and it has fewer syntactical constructions than other languages."
 Basic_Om = ("what is module","what is module.","what is module ","what is module ?","what is module?","what is module in python","what is module in python.","what is module in python?","what is module in python ?")
 Basic_AnsM = ["Consider a module to be the same as a code library.","A file containing a set of functions you want to include in your application.","A module can define functions, classes and variables. A module can also include runnable code. Grouping related code into a module makes the code easier to understand and use."]
 
@@ -123,7 +122,7 @@ def chat(user_response):
     keywordone = " module"
     keywordsecond = "module "
     
-    if(user_response!='bye'):
+    if(user_response!='bye' or user_response!='see ya!' or user_response!='see ya'):
         if(user_response=='thanks' or user_response=='thank you' ):
             flag=False
             return "You are welcome.."
@@ -133,7 +132,7 @@ def chat(user_response):
         elif(check(user_response,'are you better')=='YES'):
             flag=False
             return "I don't think so"
-        elif(user_response=='what can you do?' or user_response=='what do you do?'):
+        elif(user_response=='what can you do?' or user_response=='what do you do?' or user_response=='what can you do' or user_response=='what do you do'):
             flag=False
             return "You can ask me questions about python"
         elif(user_response=='where can I learn python?' or user_response=='where can I learn python' or user_response=='how can I learn python?') or user_response=='how can I learn python':
